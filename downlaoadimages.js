421205cc29c339a6d9885cc15dfb0454af17f4e8
Vizdun <viz@pop-os.localdomain>
2021-08-24 19:57:55 +0200

    some fixes for downloadimages.js

--- downlaoadimages.js
+++ downlaoadimages.js
@@ -3,48 +3,48 @@ var download = require("download-file")
 var fs = require("fs")
 // pass configuration object
 var client = new bot({
-  protocol: "https", // Wikipedia now enforces HTTPS
-  server: "polcompballanarchy.fandom.com", // host name of MediaWiki-powered site
-  path: "", // path to api.php script
-  debug: false, // is more verbose when set to true
+	protocol: "https", // Wikipedia now enforces HTTPS
+	server: "polcompball.fandom.com", // host name of MediaWiki-powered site
+	path: "", // path to api.php script
+	debug: false, // is more verbose when set to true
 })
 
-shitWeAlreadyHaveLolLmaoIHateThisFuckingScript = fs.readdirSync("./images")
+// uncomment in case of continued download (instead of a new one)
+
+// shitWeAlreadyHaveLolLmaoIHateThisFuckingScript = fs.readdirSync("./images")
 
 client.getPagesInNamespace(6, function (err, data) {
-  // error handling
-  if (err) {
-    console.error(err)
-    return
-  }
-  data = data.filter((item) => {
-    return (
-      shitWeAlreadyHaveLolLmaoIHateThisFuckingScript.indexOf(
-        item.title.split("File:")[1].replace(/\s/g, "_"),
-      ) === -1
-    )
-  })
-  for (p of data) {
-    client.getImageInfo(p.title, function (e, d) {
-      if (e) {
-        console.error(e)
-        return
-      }
-      if (d == null) {
-        console.log(p.title)
-        return
-      }
-      var options = {
-        directory: "./images/",
-        filename: d.descriptionurl.replace(/^http.*?\/File:/, ""),
-      }
-      download(d.url, options, function (err) {
-        if (err) throw err
+	// error handling
+	if (err) {
+		console.error(err)
+		return
+	}
+	// data = data.filter((item) => {
+	//   return (
+	//     shitWeAlreadyHaveLolLmaoIHateThisFuckingScript.indexOf(
+	//       item.title.split("File:")[1].replace(/\s/g, "_"),
+	//     ) === -1
+	//   )
+	// })
+	for (p of data) {
+		client.getImageInfo(p.title, function (e, d) {
+			if (e) {
+				console.error(e)
+				return
+			}
+			if (d == null) {
+				console.log(p.title)
+				return
+			}
+			var options = {
+				directory: "./images/",
+				filename: d.descriptionurl.replace(/^http.*?\/File:/, ""),
+			}
+			download(d.url, options, function (err) {
+				if (err) throw err
 
-        console.log(
-          `downloaded ${options.filename}`,
-        )
-      })
-    })
-  }
+				console.log(`downloaded ${options.filename}`)
+			})
+		})
+	}
 })
