288032a75dd9f6949e61d133450fd08a07566b97
Vizdun <viz@pop-os.localdomain>
2021-08-30 20:26:24 +0200

    addcomments

---
+++ scripts/useful/addcomments.js
@@ -0,0 +1,72 @@
+const fs = require("fs")
+
+const login = JSON.parse(fs.readFileSync("login.json").toString())
+
+const { mwn } = require("mwn")
+
+async function actualRoutine() {
+	const bot = await mwn.init({
+		apiUrl: "https://polcompball.miraheze.org/w/api.php",
+
+		// Can be skipped if the bot doesn't need to sign in
+		username: login.username,
+		password: login.password,
+
+		// Set your user agent (required for WMF wikis, see https://meta.wikimedia.org/wiki/User-Agent_policy):
+		userAgent: "vizbot",
+
+		// Set default parameters to be sent to be included in every API request
+		defaultParams: {
+			assert: "user", // ensure we're logged in
+		},
+	})
+
+	bot.setOptions({
+		silent: true, // suppress messages (except error messages)
+		retryPause: 5000, // pause for 5000 milliseconds (5 seconds) on maxlag error.
+		maxRetries: 3, // attempt to retry a failing requests upto 3 times
+	})
+
+	var pagesweknowthereare = []
+
+	for await (const i of [0]) {
+		try {
+			for await (let json of bot.continuedQueryGen({
+				action: "query",
+				list: "allpages",
+				aplimit: "max",
+				apnamespace: i,
+			})) {
+				let pages = json.query.allpages.map((page) => page.title)
+				pagesweknowthereare = pagesweknowthereare.concat(pages)
+			}
+		} catch {}
+	}
+
+	bot.seriesBatchOperation(
+		pagesweknowthereare,
+		async (page, idx) => {
+			console.log("editing " + page)
+			try {
+				bot.edit(page, (rev) => {
+					let text = rev.content
+						.replace("<comments /> ", "")
+						.replace(" <comments />", "")
+						.replace("<comments />", "")
+						.trim()
+						.concat("\n<comments />")
+					return {
+						text: text,
+						summary: "adding comments",
+						minor: true,
+					}
+				})
+			} catch (e) {
+				console.log(e.code) + " at " + page
+			}
+		},
+		500,
+	)
+}
+
+actualRoutine()
