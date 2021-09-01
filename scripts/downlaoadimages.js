8d6e1094576bf9b1265a50ba2516933e7cbe953f
Vizdun <viz@pop-os.localdomain>
2021-09-01 23:15:34 +0200

    minor downloadimages fixes

--- scripts/downlaoadimages.js
+++ scripts/downlaoadimages.js
@@ -1,10 +1,10 @@
-var bot = require("nodemw")
-var download = require("download-file")
-var fs = require("fs")
+import bot from "nodemw"
+import download from "download-file"
+import fs from "fs"
 // pass configuration object
 var client = new bot({
 	protocol: "https", // Wikipedia now enforces HTTPS
-	server: "polcompball.fandom.com", // host name of MediaWiki-powered site
+	server: "polcompball.fandom.com/pl", // host name of MediaWiki-powered site
 	path: "", // path to api.php script
 	debug: false, // is more verbose when set to true
 })
@@ -26,7 +26,7 @@ client.getPagesInNamespace(6, function (err, data) {
 	//     ) === -1
 	//   )
 	// })
-	for (p of data) {
+	for (const p of data) {
 		client.getImageInfo(p.title, function (e, d) {
 			if (e) {
 				console.error(e)
@@ -38,7 +38,8 @@ client.getPagesInNamespace(6, function (err, data) {
 			}
 			var options = {
 				directory: "./images/",
-				filename: d.descriptionurl.replace(/^http.*?\/File:/, ""),
+				filename:
+					d.descriptionurl.split("/")[d.descriptionurl.split("/").length - 1],
 			}
 			download(d.url, options, function (err) {
 				if (err) throw err
